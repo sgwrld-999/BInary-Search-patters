7feca51c4c70b7faa27efd9203a0bae509a6abe9
sgwrld-999 <130132265+sgwrld-999@users.noreply.github.com>
2023-10-21 11:43:38 +0530

    Search in rotated array

---
+++ Search in 1D Array/SearchInRotatedArray.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MySolution {
+public:
+    int SearchInRotatedArray(vector<int>& ipVec, int low, int high, int target) {
+    int  ans = -1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (ipVec[mid] == target) {
+            ans =  mid;
+            return ans;
+        } else if (ipVec[low] < ipVec[mid]) {
+            if(ipVec[low] <= target && ipVec[mid] >= target){
+                high = mid -1 ;
+            }
+            else{
+                low = mid + 1 ;
+            }
+        } else {
+            if(ipVec[mid] <= target && ipVec[high] >= target){
+                low = mid + 1;
+            }else{
+                high = mid - 1 ;
+            }
+        }
+    }
+    return ans;
+}
+
+};
+
+int main(int argc, char const *argv[]) {
+    int sizeOfIpVec;
+    cin >> sizeOfIpVec;
+    vector<int> ipVec(sizeOfIpVec);
+    for (int i = 0; i < sizeOfIpVec; i++) {
+        cin >> ipVec[i];
+    }
+    MySolution solution;
+    int target;
+    cin >> target;
+    int result = solution.SearchInRotatedArray(ipVec, 0, sizeOfIpVec - 1, target);
+    cout << "First and Last appearences of the key is [" << result << "]"<< endl;
+    return 0;
+}
